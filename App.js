bd1419a882e3ae8bf6bd6e06e89b5171d5d2c46e
Ibrahim Dridi <ibr.dridi@gmail.com>
2021-02-02 09:21:57 +0000

    create database and chart with mock data

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import Navigator from './routes/homeStack';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('db.db');
 
 export default function App() {
+
+  // Effect hook creates ISA table
+  React.useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS isa (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dateTime TEXT NOT NULL, ratingOne INTEGER NOT NULL, ratingTwo INTEGER NOT NULL, ratingThree INTEGER NOT NULL, summary TEXT)'
+      );
+      //tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 3, -1, 1, ?)', ['Mon Jul 20 09:12:13 GMT+01:00 2020', 'this is the summary']);
+      tx.executeSql('SELECT * from isa', [], (_, { rows }) => 
+        console.log(JSON.stringify(rows))
+      );
+      //console.log(new Date().format('DD/MM/YYYY-hh:mm:ss').toString());
+    });
+  }, []);
+
   return (
+    // Renders stack navigator (home stack)
     <Navigator />
   );
 }
--- screens/isa.js
+++ screens/isa.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    Dimensions
 } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { LineChart } from 'react-native-chart-kit';
+
+const screenWidth = Dimensions.get('window').width;
+
+const data = {
+    labels: ['Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+        {
+            data: [1, 2, 5, 2, 1, 2, 1],
+            color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
+            strokeWidth: 2
+        }
+    ],
+    legend: ['Mental Workload']
+}
+
+const chartConfig = {
+    backgroundGradientFrom: '#fff',
+    backgroundGradientTo: '#fff',
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    decimalPlaces: 0,
+    useShadowColorFromDataset: false // optional
+  };
 
 export default function ISA() {
     return (
         <View style={globalStyles.container}>
-            <Text>ISA</Text>
+            <Text>Mental Workload - 7 Days</Text>
+            <LineChart
+                data={data}
+                width={screenWidth - 40}
+                height={220}
+                chartConfig={chartConfig}
+                bezier
+                style={globalStyles.chart}
+            />
         </View>
     )
 }
