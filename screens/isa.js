2bd3c64394e9f7d9c678aeeb72d328c30a0002a9
Ibrahim Dridi <ibr.dridi@gmail.com>
2021-02-17 10:40:42 +0000

    retrieving all rows from isa table

--- screens/isa.js
+++ screens/isa.js
@@ -6,20 +6,11 @@ import {
 } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { LineChart } from 'react-native-chart-kit';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('db.db');
 
 const screenWidth = Dimensions.get('window').width;
-
-const data = {
-    labels: ['Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun'],
-    datasets: [
-        {
-            data: [1, 2, 5, 2, 1, 2, 1],
-            color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
-            strokeWidth: 2
-        }
-    ],
-    legend: ['Mental Workload']
-}
 
 const chartConfig = {
     backgroundGradientFrom: '#fff',
@@ -31,6 +22,33 @@ const chartConfig = {
   };
 
 export default function ISA() {
+
+    // Effect hook to retrieve data from database
+    React.useEffect(() => {
+    db.transaction(tx => {
+        tx.executeSql('SELECT * from isa', [], (_, { rows }) => {
+            var graphData = JSON.stringify(rows);
+            console.log(graphData);
+
+            //save the relevant columns in lists
+        });
+    });
+    }, []);
+
+    const data = {
+        // Change label to the time from retrieved rows maybe?
+        labels: ['Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun'],
+        datasets: [
+            {
+                // Replace data here with the ratingOne values from retrieved rows
+                data: [1, 2, 5, 2, 1, 2, 1],
+                color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
+                strokeWidth: 2
+            }
+        ],
+        legend: ['Mental Workload']
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text>Mental Workload - 7 Days</Text>
