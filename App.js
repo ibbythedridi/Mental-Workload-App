eca415b5ee452438abf6ea7f00f6dbc92fbd8ff1
Ibrahim Dridi <ibr.dridi@gmail.com>
2021-02-20 23:26:29 +0000

    create sleep table

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default function App() {
     db.transaction(tx => {
       tx.executeSql('CREATE TABLE IF NOT EXISTS isa (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dateTime TEXT NOT NULL, ratingOne INTEGER NOT NULL, ratingTwo INTEGER NOT NULL, ratingThree INTEGER NOT NULL, summary TEXT)'
       );
+      tx.executeSql('CREATE TABLE IF NOT EXISTS sleep (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, timeInBed REAL NOT NULL, timeTilSleep INTEGER NOT NULL, timesWokenUp INTEGER NOT NULL, sleepQuality INTEGER NOT NULL');
       // tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 4, -1, 1, ?)', ['20/07/2020-10:12:13', 'this is the summary 2']);
       // tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 2, -1, 1, ?)', ['20/07/2020-11:12:13', 'this is the summary 3']);
       // tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 1, -1, 1, ?)', ['20/07/2020-12:12:13', 'this is the summary 4']);
