85e318a569961f570fc753db1004e480a0a44f8a
Ibrahim Dridi <ibr.dridi@gmail.com>
2021-02-21 11:50:34 +0000

    fixed create statement

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ export default function App() {
       tx.executeSql('CREATE TABLE IF NOT EXISTS isa (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dateTime TEXT NOT NULL, ratingOne INTEGER NOT NULL, ratingTwo INTEGER NOT NULL, ratingThree INTEGER NOT NULL, summary TEXT)'
       );
       // timeInBed, timeTilSleep in hours (consider changing to mins)
-      tx.executeSql('CREATE TABLE IF NOT EXISTS sleep (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, date TEXT NOT NULL, timeInBed REAL NOT NULL, timeTilSleep REAL NOT NULL, timesWokenUp INTEGER NOT NULL, sleepQuality INTEGER NOT NULL');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS sleep (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, date TEXT NOT NULL, timeInBed REAL NOT NULL, timeTilSleep REAL NOT NULL, timesWokenUp INTEGER NOT NULL, sleepQuality INTEGER NOT NULL)'
+      );
       // tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 4, -1, 1, ?)', ['20/07/2020-10:12:13', 'this is the summary 2']);
       // tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 2, -1, 1, ?)', ['20/07/2020-11:12:13', 'this is the summary 3']);
       // tx.executeSql('INSERT INTO isa (dateTime, ratingOne, ratingTwo, ratingThree, summary) values (?, 1, -1, 1, ?)', ['20/07/2020-12:12:13', 'this is the summary 4']);
@@ -27,11 +28,15 @@ export default function App() {
       // tx.executeSql('INSERT INTO sleep (date, timeInBed, timeTilSleep, timesWokenUp, sleepQuality) values (?, 8, 1, 1, 2)', ['22/07/2020']);
       // tx.executeSql('INSERT INTO sleep (date, timeInBed, timeTilSleep, timesWokenUp, sleepQuality) values (?, 8.5, 0, 0, 3)', ['23/07/2020']);
       // tx.executeSql('INSERT INTO sleep (date, timeInBed, timeTilSleep, timesWokenUp, sleepQuality) values (?, 9.5, 0.5, 2, 5)', ['24/07/2020']);
-      
+
       // tx.executeSql('SELECT * from isa', [], (_, { rows }) => 
       //   console.log(JSON.stringify(rows))
       // );
-      //console.log(new Date().format('DD/MM/YYYY-hh:mm:ss').toString());
+
+      // tx.executeSql('SELECT * from sleep', [], (_, { rows }) => 
+      //   console.log(JSON.stringify(rows))
+      // );
+
     });
   }, []);
 
