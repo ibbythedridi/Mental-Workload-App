2a2a83d5ca667fddea47790fe9c560e02700640f
Ibrahim Dridi <ibr.dridi@gmail.com>
2021-02-21 20:49:38 +0000

    other sleep data added and tables updated

--- screens/sleep.js
+++ screens/sleep.js
@@ -19,46 +19,96 @@ const chartConfig = {
     useShadowColorFromDataset: false // optional
 };
 
-var graphData = [];
+const timeInBedData = {
+    labels: [],
+    datasets: [
+        {
+            data: [],
+            color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
+            strokeWidth: 2
+        }
+    ],
+    legend: ['Hours in Bed']
+}
+
+const timeTilSleepData = {
+    labels: [],
+    datasets: [
+        {
+            data: [],
+            color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
+            strokeWidth: 2
+        }
+    ],
+    legend: ['Hours until sleep']
+}
+
+const timesWokenUpData = {
+    labels: [],
+    datasets: [
+        {
+            data: [],
+            color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
+            strokeWidth: 2
+        }
+    ],
+    legend: ['Times woken up throughout the night']
+}
+
+const sleepQualityData = {
+    labels: [],
+    datasets: [
+        {
+            data: [],
+            color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
+            strokeWidth: 2
+        }
+    ],
+    legend: ['Sleep Quality Rating']
+}
+
 var graphLabels = [];
+var timeInBed = [];
+var timeTilSleep = [];
+var timesWokenUp = [];
+var sleepQuality = [];
 
 // Retrieve data from database
 //React.useEffect(() => {
 db.transaction(tx => {
-    tx.executeSql('SELECT date, timeInBed from sleep', [], (_, { rows }) => {
+    tx.executeSql('SELECT date, timeInBed, timeTilSleep, timesWokenUp, sleepQuality from sleep', [], (_, { rows }) => {
         for (var i=0; i < (rows._array.length); i++) {
-            graphData.push(rows._array[i].timeInBed);
             graphLabels.push(rows._array[i].date.slice(0, 5));
+            timeInBed.push(rows._array[i].timeInBed);
+            timeTilSleep.push(rows._array[i].timeTilSleep);
+            timesWokenUp.push(rows._array[i].timesWokenUp);
+            sleepQuality.push(rows._array[i].sleepQuality);
         }
     });
 });
 //}, []);
 
 export default function Sleep() {
-    const data = {
-        labels: [],
-        datasets: [
-            {
-                data: [],
-                color: (opacity = 1) => `rgba(20, 20, 20, ${opacity})`,
-                strokeWidth: 2
-            }
-        ],
-        legend: ['Hours in Bed']
-    }
+    var graphWidth = graphLabels.length*80;
 
-    data.datasets[0].data = graphData;
-    data.labels = graphLabels;
+    timeInBedData.labels = graphLabels;
+    timeInBedData.datasets[0].data = timeInBed;
 
-    var graphWidth = graphData.length*80;
+    timeTilSleepData.labels = graphLabels;
+    timeTilSleepData.datasets[0].data = timeTilSleep;
+
+    timesWokenUpData.labels = graphLabels;
+    timesWokenUpData.datasets[0].data = timesWokenUp;
+
+    sleepQualityData.labels = graphLabels;
+    sleepQualityData.datasets[0].data = sleepQuality;    
 
     return (
-        <View style={globalStyles.container}>
-            <Text> All recorded times in bed </Text>
+        <ScrollView style={globalStyles.container}>
             <View style={globalStyles.chart}>
                 <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                     <LineChart
-                        data={data}
+                        data={timeInBedData}
                         width={graphWidth}
                         height={220}
                         chartConfig={chartConfig}
@@ -67,6 +117,42 @@ export default function Sleep() {
                     />
                 </ScrollView>
             </View>
-        </View>
+            <View style={globalStyles.chart}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <LineChart
+                        data={timeTilSleepData}
+                        width={graphWidth}
+                        height={220}
+                        chartConfig={chartConfig}
+                        bezier
+                        style={{borderRadius: 10}}
+                    />
+                </ScrollView>
+            </View>
+            <View style={globalStyles.chart}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <LineChart
+                        data={timesWokenUpData}
+                        width={graphWidth}
+                        height={220}
+                        chartConfig={chartConfig}
+                        bezier
+                        style={{borderRadius: 10}}
+                    />
+                </ScrollView>
+            </View>
+            <View style={globalStyles.chart}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <LineChart
+                        data={sleepQualityData}
+                        width={graphWidth}
+                        height={220}
+                        chartConfig={chartConfig}
+                        bezier
+                        style={{borderRadius: 10}}
+                    />
+                </ScrollView>
+            </View>
+        </ScrollView>
     )
 }
