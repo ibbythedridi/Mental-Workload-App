00d95e24ffe4ae77b496f555af28ad7c903d4f03
Ibrahim Dridi <ibr.dridi@gmail.com>
2021-03-16 00:03:33 +0000

    date picking half working, queryDB function returning before the code has been complete however

--- screens/isa.js
+++ screens/isa.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     View,
     Text,
-    ScrollView
+    ScrollView,
+    Button
 } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { VictoryChart, VictoryGroup, VictoryLine, VictoryScatter, VictoryTheme } from 'victory-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import Moment from 'moment';
 import * as SQLite from 'expo-sqlite';
 
 const db = SQLite.openDatabase('db.db');
@@ -14,26 +17,77 @@ var graphData = [];
 
 // Retrieve data from database
 //React.useEffect(() => {
-db.transaction(tx => {
-    tx.executeSql('SELECT workloadRating, dateTime from isa', [], (_, { rows }) => {
-        for (var i=0; i < rows._array.length; i++) {
-            graphData.push({
-                x: rows._array[i].dateTime.slice(11, 16),
-                y: rows._array[i].workloadRating
-            });
-        }
+function queryDB(date) {
+    let data = [];
+    db.transaction(tx => {
+        tx.executeSql('SELECT workloadRating, dateTime from isa', [], (_, { rows }) => {
+            for (var i=0; i < rows._array.length; i++) {
+                if (rows._array[i].dateTime.slice(0, 10) == Moment(date).format('DD/MM/YYYY')) {
+                    console.log("matching");
+                    data.push({
+                        x: rows._array[i].dateTime.slice(11, 16),
+                        y: rows._array[i].workloadRating
+                    });    
+                }
+            }
+        });
     });
-});
+    /* Returning before full completion of code */
+    console.log("returning");
+    return data;
+}
+
 //}, []);
 
 export default function ISA() {
 
     var graphWidth = graphData.length*60;
 
+    const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState('date');
+    const [showPicker, setShowPicker] = useState(false);
+    const [showChart, setShowChart] = useState(false);
+
+    //console.log(Moment(date).format('DD/MM/YYYY'));
+    
+    //console.log(showPicker);
+
+    const onChange = async (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShowPicker(Platform.OS === 'ios');
+        setDate(currentDate);
+        graphData = await queryDB(date);
+        console.log(graphData.length);
+        if (graphData.length > 0) setShowChart(true);
+        else setShowChart(false);
+    }
+
+    const showMode = (currentMode) => {
+        setShowPicker(true);
+        setMode(currentMode);
+    };
+    
+    const showDatePicker = () => {
+        showMode('date');
+    };
+
     return (
         <View style={globalStyles.container}>
             <Text> All recorded mental workload levels</Text>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
+            <View>
+                <Button onPress={showDatePicker} title='Pick date' />
+            </View>
+            {showPicker && (<DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChange}
+                />
+            )}
+            {/* ScrollView for horizontal scrolling when there's lots of data */}
+            {showChart && (<ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
                 <VictoryChart 
                     theme={VictoryTheme.material} 
                     height = {300} 
@@ -49,6 +103,10 @@ export default function ISA() {
                     </VictoryGroup>
                 </VictoryChart>
             </ScrollView>
+            )}
+            {!showChart && (
+                <Text>No Data For {Moment(date).format('DD/MM/YYYY')}</Text>
+            )}
         </View>
     )
 }
